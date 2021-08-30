9842c08f0d22edab3c95b6d772c435c05698a545
benyankovich <ben.yankovich@gmail.com>
2021-08-30 19:45:43 +0300

    server fix

--- server/server.js
+++ server/server.js
@@ -13,7 +13,7 @@ connectDB()
 
 const app = express()
 
-app.use(express.json({extended: false}))
+app.use(express.json())
 
 app.use('/api/products', productRoutes)
 app.use('/api/users', userRoutes)
